9154e0203c525c8d708694cdfdce4fea94ddbeb2
charles-desouza <charles5388@gmail.com>
2020-04-04 16:19:48 +0530

    added variants

--- vue-starter-demo/js/main.js
+++ vue-starter-demo/js/main.js
@@ -4,6 +4,16 @@ var app = new Vue({
         product : 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         inStock: true,
-        //inventory: 50
+        //inventory: 50,
+        onSale: true,
+        details: ["80% cotton","20% polyster","gender neutral"],
+        variants : [{
+            variantId : 2234,
+            variantColor: "green"
+        },
+        {
+            variantId : 2235,
+            variantColor: "blue"
+        }]
     }
 });
