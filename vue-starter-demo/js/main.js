25487c3a7d75307f9f628119a58bca1f51f2c9ad
charles-desouza <charles5388@gmail.com>
2020-05-02 19:31:17 +0530

    Added add to cart event

--- vue-starter-demo/js/main.js
+++ vue-starter-demo/js/main.js
@@ -26,9 +26,6 @@ Vue.component('product',{
 
                 <button @click="addToCart()" :disabled="!inStock" :class="{disabledButton : !inStock}">Add to Cart</button>
 
-                <div class="cart">
-                    <p >Cart {{cart}}</p>
-                </div>
             </div>
 
         </div>
@@ -63,7 +60,7 @@ Vue.component('product',{
     },
     methods: {
         addToCart: function(){
-            this.cart = this.cart  + 1;
+            this.$emit('add-to-cart',this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index){
             this.selectedVariant = index;
@@ -93,6 +90,12 @@ Vue.component('product',{
 var app = new Vue({
     el: '#app', 
     data: {
-        premium : true
+        premium : true,
+        cart: []
+    },
+    methods:{
+        updateCart(id){
+            this.cart.push(id);
+        }
     }  
  });
