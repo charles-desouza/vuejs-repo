b972f87c290c4cb304c67fb636a767d97ab38c8f
charles-desouza <charles5388@gmail.com>
2020-05-02 11:49:55 +0530

    computed properties

--- vue-starter-demo/js/main.js
+++ vue-starter-demo/js/main.js
@@ -2,20 +2,21 @@ var app = new Vue({
     el: '#app',
     data : {
         product : 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: true,
-        //inventory: 50,
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         onSale: true,
         details: ["80% cotton","20% polyster","gender neutral"],
         variants : [{
             variantId : 2234,
             variantColor: 'green',
-            variantImage : './assets/vmSocks-green-onWhite.jpg'
+            variantImage : './assets/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10,
         },
         {
             variantId : 2235,
             variantColor: 'blue',
-            variantImage : './assets/vmSocks-blue-onWhite.jpg'
+            variantImage : './assets/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0,
         }],
         cart: 0
     },
@@ -23,8 +24,19 @@ var app = new Vue({
         addToCart: function(){
             this.cart = this.cart  + 1;
         },
-        updateProduct(variantImage){
-            this.image = variantImage;
+        updateProduct(index){
+            this.selectedVariant = index;
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product;
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
         }
     }
 });
